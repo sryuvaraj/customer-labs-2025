f3f9cc066279f8d031c9a6b29f4fc45e4a50aaf6
sryuvaraj <sryuvaraj99@gmail.com>
2025-11-09 20:01:52 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { useParams } from 'react-router-dom'
 import './App.css'
-import NavBar from './components/NavBar'
-import Pagination from './components/Pagination'
-import Routets from './components/Routets'
-import ViewAudience from './components/ViewAudience'
 import { useState } from 'react'
 
 function App() {
 
   return (
     <div className='h-100vh'>
-    {/* <ViewAudience /> */}
-    {/* <Pagination /> */}
-    <NavBar />
-    <Routets />
     </div>
   )
 }
--- src/components/Subages/AboutTwo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutTwo = () => {
-  return (
-    <div>AboutTwo</div>
-  )
-}
-
-export default AboutTwo
